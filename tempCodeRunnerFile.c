46c962dbe6d642de16ddbadea82592d633c3a796
vedant Kale <vedantkale0707@gmail.com>
2025-04-17 12:35:58 +0530

    stack and codeRunnerFile is added

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+sum();
+int sum(int a, int b)
+{
+    int add;
+    add= a+b;
+}
+int main()
+{
+    int a ,b;
+    printf("Enter the value of a and b: ");
+    scanf("%d %d",&a,&b);
+
+   sum();
+}
